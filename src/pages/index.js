6356ec7ad599a82315b4d160e769c1e4475ce3ff
Vladislav Maraev <vlad@maraev.me>
2020-03-30 15:09:10 +0200

    redirect from gatsby index

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,22 +6,17 @@ import { Link } from "gatsby"
 
 const IndexPage = () => {
   return (
-    <Layout>
-      <div>
-        <div align="center">
-        <br/>
-          <p style={{color: "cornflowerblue", fontSize: 50, fontWeight: 'bold'}}>
-            Gatsby Markdown Starter
-          </p>
-          <h2>Boilerplate for markdown-based website</h2>
-          <br/>
-          <Link to="/docs/get-started/introduction">
-            <Button type="primary" size="large" icon="right-circle" style={{marginRight: 10}}>Get Started</Button>
-          </Link>
-          <Button type="primary" size="large" icon="github" href="https://github.com/cvluca/gatsby-starter-markdown">Github</Button>
-        </div>
-      </div>
-    </Layout>
+<html>
+   <head>
+      <title>Redirect to CLASP.gu.se</title>
+      <meta http-equiv = "refresh" content = "0; url = https://clasp.gu.se/" />
+   </head>
+   <body>
+      <p>Redirect to <a href="https://clasp.gu.se/">clasp.gu.se</a></p>
+   </body>
+</html>
+
+
   )
 }
 
