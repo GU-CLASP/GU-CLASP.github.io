8eedd545a80af303bf1fff882e41f0be5c2810e1
aram karimi <aram.karimi68@gmail.com>
2020-08-18 16:47:24 +0200

    add pagination to event pages

--- src/pages/event.js
+++ src/pages/event.js
@@ -35,7 +35,7 @@ export default function Event(props) {
       break;
     case "seminars":
       var seminars = props.data.seminars.seminars
-      var numPages = Math.ceil(props.data.seminars.totalCount / postsPerPage)
+      var numPagesSeminars = Math.ceil(props.data.seminars.totalCount / postsPerPage)
       var page = search.page
       if (isNaN(page)) {
         page = 1
@@ -57,63 +57,21 @@ export default function Event(props) {
                   return getSeminar(seminar)
                 }
               })}
-              <ul class="pagination" style={{
-                display: 'flex',
-                flexWrap: 'wrap',
-                justifyContent: 'center',
-                alignItems: 'center',
-                listStyle: 'none',
-                padding: 0,
-              }}>
-                {page == 1 ?
-                  <li class="page-item disabled">
-                    <a class="page-link" href={"/event?type=seminars&page=" + (page - 1)} aria-label="Previous">
-                      <span aria-hidden="true">&laquo;</span>
-                      <span class="sr-only">Previous</span>
-                    </a>
-                  </li>
-                  :
-                  <li class="page-item">
-                    <a class="page-link" href={"/event?type=seminars&page=" + (page - 1)} aria-label="Previous">
-                      <span aria-hidden="true">&laquo;</span>
-                      <span class="sr-only">Previous</span>
-                    </a>
-                  </li>
-                }
-
-                {Array.apply(0, Array(numPages)).map(function (x, i) {
-                  var href = "/event?type=seminars&page=" + (i + 1)
-                  if (page == (i + 1)) {
-                    return (<li class="page-item active"><a class="page-link" href={href}>{i + 1}</a></li>)
-                  }
-                  else {
-                    return (<li class="page-item"><a class="page-link" href={href}>{i + 1}</a></li>)
-                  }
-                })}
-                {page == numPages ?
-                  <li class="page-item disabled">
-                    <a class="page-link" href={"/event?type=seminars&page=" + (parseInt(page) + 1)} aria-label="Previous">
-                      <span aria-hidden="true">&raquo;</span>
-                      <span class="sr-only">Next</span>
-                    </a>
-                  </li>
-                  :
-                  <li class="page-item">
-                    <a class="page-link" href={"/event?type=seminars&page=" + (parseInt(page) + 1)} aria-label="Previous">
-                      <span aria-hidden="true">&raquo;</span>
-                      <span class="sr-only">Next</span>
-                    </a>
-                  </li>
-                }
-              </ul>
+              {getPagination("/event?type=seminars", numPagesSeminars, page)}
             </div>
           }
-
         </div>
       )
       break;
     case "conference":
       var conferences = props.data.conferences.conferences
+      var numPagesConferences = Math.ceil(props.data.conferences.totalCount / postsPerPage)
+      var page = search.page
+      if (isNaN(page)) {
+        page = 1
+      }
+      var upperBound = (page * postsPerPage) - 1
+      var lowerBound = (page - 1) * postsPerPage
       var isEmpty = props.data.conferences.totalCount > 0 ? false : true
       content = (
         <div>
@@ -123,10 +81,14 @@ export default function Event(props) {
             </div>
             :
             <div>
+
               {conferences.map((conference, index) => {
-                conference = conference.conference
-                return getConference(conference)
+                if (index <= upperBound && index >= lowerBound) {
+                  conference = conference.conference
+                  return getConference(conference)
+                }
               })}
+              {getPagination("/event?type=confrances", numPagesConferences, page)}
             </div>
           }
         </div>
@@ -134,6 +96,13 @@ export default function Event(props) {
       break;
     case "workshops":
       var workshops = props.data.workshops.workshops
+      var numPagesWorkshops = Math.ceil(props.data.workshops.totalCount / postsPerPage)
+      var page = search.page
+      if (isNaN(page)) {
+        page = 1
+      }
+      var upperBound = (page * postsPerPage) - 1
+      var lowerBound = (page - 1) * postsPerPage
       var isEmpty = props.data.workshops.totalCount > 0 ? false : true
       content = (
         <div>
@@ -144,9 +113,12 @@ export default function Event(props) {
             :
             <div>
               {workshops.map((workshop, index) => {
-                workshop = workshop.workshop
-                return getWorkshop(workshop)
+                if (index <= upperBound && index >= lowerBound) {
+                  workshop = workshop.workshop
+                  return getWorkshop(workshop)
+                }
               })}
+              {getPagination("/event?type=workshops", numPagesWorkshops, page)}
             </div>
           }
         </div>
@@ -161,6 +133,200 @@ export default function Event(props) {
       {content}
     </Layout>
   )
+}
+
+function getPagination(link, number_of_pages, current_page) {
+  current_page = parseInt(current_page)
+  number_of_pages = parseInt(number_of_pages)
+  if (number_of_pages <= 5) {
+    return (
+      <ul class="pagination" style={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+        alignItems: 'center',
+        listStyle: 'none',
+        padding: 0,
+      }}>
+        {current_page == 1 ?
+          <li class="page-item disabled">
+            <a class="page-link pagination-prev" href={link + "&page=" + (current_page - 1)} aria-label="Previous">
+              <span aria-hidden="true">&laquo;</span>
+              <span class="sr-only">Previous</span>
+            </a>
+          </li>
+          :
+          <li class="page-item">
+            <a class="page-link pagination-next" href={link + "&page=" + (current_page - 1)} aria-label="Previous">
+              <span aria-hidden="true">&laquo;</span>
+              <span class="sr-only">Previous</span>
+            </a>
+          </li>
+        }
+
+        {Array.apply(0, Array(number_of_pages)).map(function (x, i) {
+          var href = link + "&page=" + (i + 1)
+          if (current_page == (i + 1)) {
+            return (<li class="page-item active"><a class="page-link" href={href}>{i + 1}</a></li>)
+          }
+          else {
+            return (<li class="page-item"><a class="page-link" href={href}>{i + 1}</a></li>)
+          }
+        })}
+        {current_page == number_of_pages ?
+          <li class="page-item disabled">
+            <a class="page-link pagination-next" href={link + "&page=" + (current_page + 1)} aria-label="Next">
+              <span aria-hidden="true">&raquo;</span>
+              <span class="sr-only">Next</span>
+            </a>
+          </li>
+          :
+          <li class="page-item">
+            <a class="page-link pagination-next" href={link + "&page=" + (current_page + 1)} aria-label="Next">
+              <span aria-hidden="true">&raquo;</span>
+              <span class="sr-only">Next</span>
+            </a>
+          </li>
+        }
+      </ul>
+    )
+  }
+  else {
+    var items = []
+
+    if (current_page == 1) {
+      items.push(
+        <li class="page-item disabled">
+          <a class="page-link pagination-prev" href={link + "&page=" + (current_page - 1)} aria-label="Previous">
+            <span aria-hidden="true">&laquo;</span>
+            <span class="sr-only">Previous</span>
+          </a>
+        </li>
+      )
+
+      items.push(
+        <li class="page-item active">
+          <a class="page-link" href={link + "&page=1"}>1</a>
+        </li>
+      )
+    }
+    else {
+      items.push(
+        <li class="page-item">
+          <a class="page-link pagination-prev" href={link + "&page=" + (current_page - 1)} aria-label="Previous">
+            <span aria-hidden="true">&laquo;</span>
+            <span class="sr-only">Previous</span>
+          </a>
+        </li>
+      )
+
+      items.push(
+        <li class="page-item">
+          <a class="page-link" href={link + "&page=1"}>1</a>
+        </li>
+      )
+    }
+    //////////////////////////////////////////////////////
+    if (current_page > 4) {
+      items.push(
+        <li class="page-item disabled">
+          <a class="page-link" href="#">...</a>
+        </li>
+      )
+
+      for (var i = (current_page - 1); i <= (current_page + 1); i++) {
+        if (i >= number_of_pages)
+          continue
+        if (current_page == i) {
+          items.push(
+            <li class="page-item active">
+              <a class="page-link" href={link + "&page=" + i}>{i}</a>
+            </li>
+          )
+        }
+        else {
+          items.push(
+            <li class="page-item">
+              <a class="page-link" href={link + "&page=" + i}>{i}</a>
+            </li>
+          )
+        }
+      }
+    }
+    else {
+      for (var i = 2; i <= (current_page + 1); i++) {
+        if (current_page == i) {
+          items.push(
+            <li class="page-item active">
+              <a class="page-link" href={link + "&page=" + i}>{i}</a>
+            </li>
+          )
+        }
+        else {
+          items.push(
+            <li class="page-item">
+              <a class="page-link" href={link + "&page=" + i}>{i}</a>
+            </li>
+          )
+        }
+      }
+    }
+
+    if (current_page <= (number_of_pages - 3)) {
+      items.push(
+        <li class="page-item disabled">
+          <a class="page-link" href="#">...</a>
+        </li>
+      )
+    }
+    /////////////////////////////////////////////////////////////////
+    if (current_page == number_of_pages) {
+      items.push(
+        <li class="page-item active">
+          <a class="page-link" href={link + "&page=" + number_of_pages}>{number_of_pages}</a>
+        </li>
+      )
+
+      items.push(
+        <li class="page-item disabled">
+          <a class="page-link pagination-next" href={link + "&page=" + (current_page + 1)} aria-label="Next">
+            <span aria-hidden="true">&raquo;</span>
+            <span class="sr-only">Next</span>
+          </a>
+        </li>
+      )
+    }
+    else {
+      items.push(
+        <li class="page-item">
+          <a class="page-link" href={link + "&page=" + number_of_pages}>{number_of_pages}</a>
+        </li>
+      )
+
+      items.push(
+        <li class="page-item">
+          <a class="page-link pagination-next" href={link + "&page=" + (current_page + 1)} aria-label="Next">
+            <span aria-hidden="true">&raquo;</span>
+            <span class="sr-only">Next</span>
+          </a>
+        </li>
+      )
+    }
+
+    return (
+      <ul class="pagination" style={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+        alignItems: 'center',
+        listStyle: 'none',
+        padding: 0,
+      }}>
+        {items}
+      </ul>
+    )
+  }
+
 }
 
 function getSeminar(seminar) {
