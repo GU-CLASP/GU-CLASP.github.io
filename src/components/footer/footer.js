8caaec9ad958a9f6dc0fa2b5470e2ad25237c1c3
AramKarimi <aram.karimi@gu.se>
2022-09-09 14:10:30 +0200

    update footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -9,7 +9,7 @@ export default function FooterComponent({ props }) {
           <div class="footer-header-type-style">Contact Information</div>
           <p><b>Office Hours: </b>Monday-Friday 9.00am - 5.00pm</p>
           <p><b>Phone: </b> +46 31-786 0000</p>
-          <p><b>E-mail: </b><a href="mailto:hanna.edblom@gu.se"> Hanna Edblom</a></p>
+          <p><b>E-mail: </b><a href="mailto:susanna.myyry@gu.se"> Susanna Myyry</a></p>
         </Col>
         <Col className="col-12 col-md-4">
           <div class="footer-header-type-style">Quick Links</div>
